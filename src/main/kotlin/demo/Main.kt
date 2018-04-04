fa22b2bbba898c5690249e1816bf1259b8e17362
ringo-apo <ringo1625@gmail.com>
2018-04-05 08:32:39 +0900

    201804050839

---
+++ src/main/kotlin/demo/Main.kt
@@ -0,0 +1,15 @@
+package demo
+
+fun main(args: Array<String>) {
+    println("hello")
+
+    for (i in 1..100) {
+        val num = when {
+            i % 15 == 0 -> "FizzBuzz"
+            i % 3 == 0 -> "Fizz"
+            i % 5 == 0 -> "Buzz"
+            else -> "$i"
+        }
+        println(num)
+    }
+}
